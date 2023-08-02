8a342f9b06d4143d9bcb175428888c04e12bf011
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-08-02 12:39:48 -0400

    Update JiraResponseTP.sh

--- JiraResponseTP.sh
+++ JiraResponseTP.sh
@@ -19,20 +19,17 @@ url='https://xray.cloud.getxray.app/api/v2/graphql'
 # La consulta GraphQL
 # Define la consulta GraphQL
 query='{
-    "query": "{ getTestPlans(jql: \"project = 'PX'\", limit: 20) { results { issueId jira(fields: [\"summary\", \"customfield_10037\", \"customfield_10106\", \"customfield_10107\", \"customfield_10108\"])
-        }
-    }
-}"
+    "query": "{ getTestPlans(jql: \"project = PX\", limit: 1 ) { results { issueId jira(fields: [\"summary\", \"customfield_10037\", \"customfield_10106\",\"custom>
 }'
 
 # Realizamos la petición con curl y guardamos la respuesta
-response1=$(curl --silent -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d "$query" $url) > result.json
+response1=$(curl --silent -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d "$query" $url)
 
 # Imprime la respuesta
-issueIds1=$(cat result.json | jq -r '.data.getTestPlan.jira.customfield_10037')
-issueIds2=$(cat result.json | jq -r '.data.getTestPlan.jira.customfield_10106')
-issueIds3=$(cat result.json | jq -r '.data.getTestPlan.jira.customfield_10107')
-issueIds4=$(cat result.json | jq -r '.data.getTestPlan.jira.customfield_10108')
+issueIds1=$(echo response1 | jq -r '.data.getTestPlan.jira.customfield_10037')
+issueIds2=$(echo response1 | jq -r '.data.getTestPlan.jira.customfield_10106')
+issueIds3=$(echo response1 | jq -r '.data.getTestPlan.jira.customfield_10107')
+issueIds4=$(echo response1 | jq -r '.data.getTestPlan.jira.customfield_10108')
 echo $issueIds1
 echo $issueIds2
 echo $issueIds3
